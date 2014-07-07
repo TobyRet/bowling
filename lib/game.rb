e702744f46a9f88976a93ec96d97a6881c3c6664
Toby Retallick <toby.retallick@gmail.com>
2014-07-07 23:35:24 +0100

    Initialise new instance of Game with player and empty bowl tally

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,13 @@
 
+class Game
+
+  attr_reader :bowls
+
+  def initialize(player)
+    @player = player
+    @bowls = []
+    10.times { @bowls << ["", ""] }
+  end
+
+
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,17 @@
 require 'spec_helper'
-require 'game'
+require './lib/game'
+
+describe 'Game logic' do
+
+  it 'initializes a new game with a single player and an empty bowl tally' do
+
+    player = "Toby"
+
+    game = Game.new(player)
+
+    expect(game.bowls).to be_a(Array)
+
+  end
+
+
+end

d4c242e6c04bc44aeda1438dafd175b7f4add64e
Toby Retallick <toby.retallick@gmail.com>
2014-07-09 00:37:03 +0100

    Game calculates spares correctly

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,32 @@
 
 class Game
 
-  attr_reader :bowls
+  attr_reader :score
 
   def initialize
-    @score = 0
+    @bowls = []
   end
 
   def bowl(pins)
-    @score = score + pins
+    @bowls << pins
   end
 
   def score
-    @score
+    score = 0
+    frame = 0
+    i = 0
+
+    while frame < 10
+      if @bowls[i] + @bowls[i+1] == 10
+        score += 10 + @bowls[i+2]
+        i += 2
+      else
+        score += (@bowls[i] + @bowls[i+1])
+        i +=2
+      end
+      frame += 1
+    end
+    score
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,12 +6,23 @@ describe 'Game logic' do
   let(:game) { Game.new }
 
   context 'initialisation' do
-    it 'initializes a new game with a score of 0' do
+    xit 'initializes a new game with a score of 0' do
       expect(game.score).to eq(0)
     end
   end
 
   context 'scoring' do
+    it 'scores a game with no strikes and spares correctly' do
+      20.times { game.bowl(4) }
 
+      expect(game.score).to eq(80)
+    end
+
+    it 'scores spares correctly' do
+      2.times { game.bowl(5) }
+      18.times { game.bowl(1) }
+
+      expect(game.score).to eq(29)
+    end
   end
 end
